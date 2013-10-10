5911cbab6423394313347f179cac34706479c70d
James Ruiz <JamesRuiz@gatech.edu>
2013-10-09 22:39:04 -0400

    Add Comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,8 @@
-
+/*
+*This program was created to make dreams come true.
+*My dream was for you to read this comment.
+*Therefore, dreams do come true.
+*/
 public class HelloWorld {
 
 	/**
@@ -6,7 +10,8 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World, from BARE MINIMUM STUDIOS!");
+		System.out.println("Hello World, from BARE MINIMUM STUDIOS!/n");
+		System.out.println("\nWhere we put the \"minimum\" in bare minimum!");
 
 	}
 
